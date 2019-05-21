3880d8ef59b2d75ad00016b3443823356e734597
deocksoo <dusol92@snu.ac.kr>
2019-05-21 14:21:42 +0900

    [ADD] Sonata 클래스에 주입해야할 연료량 구하는 기능 추가

--- src/main/java/rentcompany/Sonata.java
+++ src/main/java/rentcompany/Sonata.java
@@ -1,17 +1,30 @@
 package rentcompany;
 
 public class Sonata {
-    private static final int MIN_DISTANCE = 1;
-    private int tripDistance;
+    private static final double DISTANCE_PER_LITER = 10;
+    private static final double MIN_DISTANCE = 1;
+    private double tripDistance;
 
-    public Sonata(int tripDistance) {
+    public Sonata(double tripDistance) {
         validateDistance(tripDistance);
         this.tripDistance = tripDistance;
     }
 
-    private void validateDistance(int tripDistance) {
+    private void validateDistance(double tripDistance) {
         if (tripDistance < MIN_DISTANCE) {
             throw new IllegalArgumentException("양수를 입력해야 합니다");
         }
+    }
+
+    public double getChargeQuantity() {
+        return getTripDistance() / getDistancePerLiter();
+    }
+
+    private double getDistancePerLiter() {
+        return DISTANCE_PER_LITER;
+    }
+
+    private double getTripDistance() {
+        return tripDistance;
     }
 }
--- src/test/java/rentcompany/SonataTest.java
+++ src/test/java/rentcompany/SonataTest.java
@@ -2,6 +2,7 @@ package rentcompany;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SonataTest {
@@ -13,5 +14,12 @@ public class SonataTest {
     @Test
     public void 이동거리가_0일_때_예외를_던지는지_테스트() {
         assertThrows(IllegalArgumentException.class, () -> new Sonata(0));
+    }
+
+    @Test
+    void 주입해야할_연료량을_제대로_반환하는지_테스트() {
+        Sonata sonata = new Sonata(100);
+
+        assertThat(sonata.getChargeQuantity()).isEqualTo(10);
     }
 }
