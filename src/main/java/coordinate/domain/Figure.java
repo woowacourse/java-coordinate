514e2a17c080680f2b62b09e864c9544501a9507
choihz <coiio@naver.com>
2019-05-21 16:35:06 +0900

    Feat Figure Class
    Figure 및 FigureTest 작성

---
+++ src/main/java/coordinate/domain/Figure.java
@@ -0,0 +1,15 @@
+package coordinate.domain;
+
+import java.util.List;
+
+public class Figure {
+    private final List<Point> points;
+
+    public Figure(List<Point> points) {
+        this.points = points;
+    }
+
+    public String drawPlane() {
+        return null;
+    }
+}
