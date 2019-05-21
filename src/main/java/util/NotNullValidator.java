ddeeaf70be425e120c9072131b009f4e3cb20008
Yu-Min Kim <green4469@yonsei.ac.kr>
2019-05-21 16:54:41 +0900

    refactor: NotNullValidator now validates lists having null elements

--- src/main/java/util/NotNullValidator.java
+++ src/main/java/util/NotNullValidator.java
@@ -1,9 +1,15 @@
 package util;
 
+import java.util.List;
+
 public class NotNullValidator {
     public static void validateNotNull(Object o) {
-        if (o == null) {
+        if (o == null || hasNull((List) o)) {
             throw new IllegalArgumentException("생성자의 인자에 null이 입력될 수 없습니다.");
         }
+    }
+
+    private static boolean hasNull(List list) {
+        return list.contains(null);
     }
 }
--- src/test/java/util/NotNullValidatorTest.java
+++ src/test/java/util/NotNullValidatorTest.java
@@ -2,11 +2,22 @@ package util;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class NotNullValidatorTest {
     @Test
     void nullTest() {
         assertThrows(IllegalArgumentException.class, () -> NotNullValidator.validateNotNull(null));
+    }
+
+    @Test
+    void listNullTest() {
+        List<Integer> testList = new ArrayList<>();
+        testList.add(null);
+        assertThrows(IllegalArgumentException.class, () -> NotNullValidator.validateNotNull(testList));
     }
 }
