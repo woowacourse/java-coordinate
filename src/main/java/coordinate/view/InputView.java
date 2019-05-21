c5f737ac7f0912d59c706b129c5367b406a7eee0
pkch93 <star_final@naver.com>
2019-05-21 16:00:06 +0900

    feat: InputView.class 구현
    - 좌표 입력 기능

---
+++ src/main/java/coordinate/view/InputView.java
@@ -0,0 +1,12 @@
+package coordinate.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static String inputCooridinate() {
+        System.out.println("좌표를 입력하세요.");
+        return scanner.nextLine();
+    }
+}
