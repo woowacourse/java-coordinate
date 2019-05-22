251ded6916897c2aaeffd4b7ead6579a51d5efdd
men7627 <men1219@hanmail.net>
2019-05-22 10:39:35 +0900

    feat : 좌표 숫자 범위 검사

---
+++ src/main/java/coordinate/model/PointNumber.java
@@ -0,0 +1,17 @@
+package coordinate.model;
+
+public class PointNumber {
+    private static final int MIN_RANGE = 0;
+    private static final int MAX_RANGE = 24;
+    private int number;
+    public PointNumber(int number) {
+        checkValidRange(number);
+        this.number=number;
+    }
+
+    private void checkValidRange(int number) {
+        if(number<MIN_RANGE || number >MAX_RANGE){
+            throw new IllegalArgumentException("좌표 숫자 입력 범위 오류");
+        }
+    }
+}
---
+++ src/test/java/coordinate/PointNumberTest.java
@@ -0,0 +1,16 @@
+package coordinate;
+
+import coordinate.model.PointNumber;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class PointNumberTest {
+
+    @Test
+    void 좌표숫자_범위검사() {
+        assertThrows(IllegalArgumentException.class,()->{
+           new PointNumber(25);
+        });
+    }
+}
