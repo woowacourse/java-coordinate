3559cb7b510c03a7b2afaf4052999d86e93044d0
hyojaekim <hj00686@naver.com>
2019-05-22 15:38:53 +0900

    feat(Point) : Create Point class and add sub method

---
+++ src/main/java/coordinate/domain/Point.java
@@ -0,0 +1,31 @@
+package coordinate.domain;
+
+import java.util.Objects;
+
+public class Point {
+    private final int x;
+    private final int y;
+
+    Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double sub(Point secondPoint) {
+        return Math.sqrt(Math.pow(x - secondPoint.x, 2) + Math.pow(y - secondPoint.y, 2));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x &&
+                y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
