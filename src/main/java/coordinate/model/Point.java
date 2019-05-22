3c7355d3c50ee264c71224045695155089896f30
men7627 <men1219@hanmail.net>
2019-05-22 10:50:15 +0900

    feat : Point 클래스 생성

---
+++ src/main/java/coordinate/model/Point.java
@@ -0,0 +1,11 @@
+package coordinate.model;
+
+public class Point {
+    private PointNumber x;
+    private PointNumber y;
+
+    public Point(PointNumber x, PointNumber y) {
+        this.x = x;
+        this.y = y;
+    }
+}
