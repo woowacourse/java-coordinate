8e5989c2175818aa697df16cd4a578ab636193f0
men7627 <men1219@hanmail.net>
2019-05-22 16:44:32 +0900

    feat: 삼각형 넓이 계산 및 삼각형 세 점 한 직선에 있는지 검사
    * 헤론의 공식을 이용하여 삼각형 넓이 계산.
    
    * 삼각형 세 점이 한 직선에 있는 예외 테스트 코드 추가.

--- src/main/java/coordinate/model/Point.java
+++ src/main/java/coordinate/model/Point.java
@@ -2,7 +2,7 @@ package coordinate.model;
 
 import java.util.Objects;
 
-public class Point implements Comparable<Point>{
+public class Point implements Comparable<Point> {
     private PointNumber x;
     private PointNumber y;
 
@@ -13,7 +13,14 @@ public class Point implements Comparable<Point>{
 
     public double getDistance(Point point2) {
         return Math.sqrt(Math.pow(this.x.getNumber() - point2.x.getNumber(), 2)
-        + Math.pow(this.y.getNumber() - point2.y.getNumber(), 2));
+                + Math.pow(this.y.getNumber() - point2.y.getNumber(), 2));
+    }
+
+    public double getIncline(Point point) {
+        if (point.x.getNumber() == this.x.getNumber()) {
+            return Integer.MAX_VALUE;
+        }
+        return (double) (point.y.getNumber() - this.y.getNumber()) / (point.x.getNumber() - this.x.getNumber());
     }
 
     public boolean isEqualX(Point anotherPoint) {
@@ -26,7 +33,7 @@ public class Point implements Comparable<Point>{
 
     @Override
     public int compareTo(Point o) {
-        if(x.getNumber() == o.x.getNumber()){
+        if (x.getNumber() == o.x.getNumber()) {
             return y.getNumber() - o.y.getNumber();
         }
         return x.getNumber() - o.x.getNumber();
@@ -44,12 +51,5 @@ public class Point implements Comparable<Point>{
     @Override
     public int hashCode() {
         return Objects.hash(x, y);
-    }
-` `
-    public double getIncline(Point point) {
-        if(point.x.getNumber() == this.x.getNumber()){
-            return -1;
-        }
-        return (double)(point.y.getNumber() - this.y.getNumber()) / (point.x.getNumber() - this.x.getNumber());
     }
 }
