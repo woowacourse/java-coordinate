40e2085c29f59e7a8e261147f55c1aa9bdc1d9ca
Yu-Min Kim <green4469@yonsei.ac.kr>
2019-05-22 11:16:07 +0900

    refactor: more tests for class 'Point'

--- src/test/java/coordinate/domain/PointTest.java
+++ src/test/java/coordinate/domain/PointTest.java
@@ -3,9 +3,15 @@ package coordinate.domain;
 import coordinate.domain.Point;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PointTest {
+    @Test
+    void equalsTest() {
+        assertThat(new Point(0, 0)).isEqualTo(new Point(0, 0));
+    }
+
     @Test
     void xLimitOverTest() {
         assertThrows(IllegalArgumentException.class, () -> new Point(25, 10));
@@ -24,5 +30,40 @@ public class PointTest {
     @Test
     void yLimitUnderTest() {
         assertThrows(IllegalArgumentException.class, () -> new Point(10, -1));
+    }
+
+    @Test
+    void compareBiggerWhenSameYTest() {
+        Point p1 = new Point(1, 0);
+        Point p2 = new Point(0, 0);
+        assertThat(p1.compareTo(p2)).isEqualTo(1);
+    }
+
+    @Test
+    void compareBiggerWhenSameXTest() {
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(1, 0);
+        assertThat(p1.compareTo(p2)).isEqualTo(1);
+    }
+
+    @Test
+    void compareSmallerWhenSameYTest() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(1, 0);
+        assertThat(p1.compareTo(p2)).isEqualTo(-1);
+    }
+
+    @Test
+    void compareSmallerWhenSameXTest() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(0, 1);
+        assertThat(p1.compareTo(p2)).isEqualTo(-1);
+    }
+
+    @Test
+    void compareSameTest() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(0, 0);
+        assertThat(p1.compareTo(p2)).isEqualTo(0);
     }
 }
