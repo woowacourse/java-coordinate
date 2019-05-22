5f11d07db06f972f0893daa78fd24253cd5bfa84
Yu-Min Kim <green4469@yonsei.ac.kr>
2019-05-22 11:50:51 +0900

    refactor: more tests for class 'Line'

--- src/test/java/coordinate/domain/LineTest.java
+++ src/test/java/coordinate/domain/LineTest.java
@@ -9,6 +9,11 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LineTest {
     @Test
+    void nullPointTest() {
+        assertThrows(IllegalArgumentException.class, () -> new Line(null, null));
+    }
+
+    @Test
     void checkSamePointTest() {
         Point p1 = new Point(1, 2);
         Point p2 = new Point(1, 2);
@@ -21,5 +26,29 @@ public class LineTest {
         Point p2 = new Point(1, 2);
         Line line = new Line(p1, p2);
         assertThat(line.calculateDistance()).isEqualTo(Math.sqrt(2), Offset.offset(0.001));
+    }
+
+    @Test
+    void calculateSlopeTest() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(1, 1);
+        Line line = new Line(p1, p2);
+        assertThat(line.calculateSlope()).isEqualTo(1.0, Offset.offset(0.001));
+    }
+
+    @Test
+    void isHorizontalTest() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(5, 0);
+        Line line = new Line(p1, p2);
+        assertThat(line.isHorizontal()).isTrue();
+    }
+
+    @Test
+    void isVerticalTest() {
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(0, 5);
+        Line line = new Line(p1, p2);
+        assertThat(line.isVertical()).isTrue();
     }
 }
