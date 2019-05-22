64a630cc3a204a66c49c185320add093db6433d5
choihz <coiio@naver.com>
2019-05-22 11:18:04 +0900

    Test TriangleTest Class
    포인트 갯수가 적게 들어왔을 때 예외를 던지는지 확인하는 테스트 추가

--- src/test/java/coordinate/domain/TriangleTest.java
+++ src/test/java/coordinate/domain/TriangleTest.java
@@ -6,10 +6,18 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 public class TriangleTest {
+    @Test
+    public void Point가_3개_미만_들어올_경우() {
+        List<Point> points = new ArrayList<>(Arrays.asList(new Point(1, 1), new Point(2, 2)));
+
+        assertThatThrownBy(() -> {
+            new Triangle(points);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
     @Test
     public void Point가_3개_이상_들어올_경우() {
         List<Point> points = new ArrayList<>(Arrays.asList(new Point(1, 1), new Point(2, 2), new Point(3, 3), new Point(4, 4)));
@@ -21,9 +29,9 @@ public class TriangleTest {
 
     @Test
     public void 넓이를_잘_구하는지_확인() {
-        List<Point> points = new ArrayList<>(Arrays.asList(new Point(1, 1), new Point(1, 5), new Point(5, 1)));
+        List<Point> points = new ArrayList<>(Arrays.asList(new Point(1, 1), new Point(6, 1), new Point(3, 16)));
         Triangle triangle = new Triangle(points);
 
-        assertThat(triangle.area()).isEqualTo(0);
+        assertThat(triangle.area()).isEqualTo(37.5, offset(0.00099));
     }
 }
