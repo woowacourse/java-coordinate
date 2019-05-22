63c9dd3fac2882a728c5d2de7456fa54fc959968
choihz <coiio@naver.com>
2019-05-22 14:28:52 +0900

    Style FigureGenerator Class
    오타 수정

--- src/main/java/coordinate/domain/FigureGenerator.java
+++ src/main/java/coordinate/domain/FigureGenerator.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class FigureGenerator {
     private static final int NUMBER_OF_POINTS_FOR_LINE = 2;
     private static final int NUMBER_OF_POINTS_FOR_TRIANGLE = 3;
-    private static final int NUMBER_OF_POINTS_FOR_RACTANGLE = 4;
+    private static final int NUMBER_OF_POINTS_FOR_RECTANGLE = 4;
 
     public static Figure generate(List<Point> points) {
         if (points.size() == NUMBER_OF_POINTS_FOR_LINE) {
@@ -16,7 +16,7 @@ public class FigureGenerator {
             return new Triangle(points);
         }
 
-        if (points.size() == NUMBER_OF_POINTS_FOR_RACTANGLE) {
+        if (points.size() == NUMBER_OF_POINTS_FOR_RECTANGLE) {
             return new Rectangle(points);
         }
 
