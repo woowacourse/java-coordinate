5a9f0473e929eea4f2653b9f072f0b16461077c3
choihz <coiio@naver.com>
2019-05-22 14:26:42 +0900

    Feat Point Class
    equals(), hashCode() 오버라이드 및 오타 수정

--- src/main/java/coordinate/domain/Point.java
+++ src/main/java/coordinate/domain/Point.java
@@ -1,5 +1,7 @@
 package coordinate.domain;
 
+import java.util.Objects;
+
 public class Point {
     private static final int MIN_COORDINATE = 1;
     private static final int MAX_COORDINATE = 24;
@@ -15,10 +17,10 @@ public class Point {
 
     private void validateRange(int x, int y) {
         if (x < MIN_COORDINATE || x > MAX_COORDINATE) {
-            throw new IllegalArgumentException(String.format("x의 값이 법위를 벗어났습니다. x : %d", x));
+            throw new IllegalArgumentException(String.format("x의 값이 범위를 벗어났습니다. x : %d", x));
         }
         if (y < MIN_COORDINATE || y > MAX_COORDINATE) {
-            throw new IllegalArgumentException(String.format("y의 값이 법위를 벗어났습니다. y : %d", y));
+            throw new IllegalArgumentException(String.format("y의 값이 범위를 벗어났습니다. y : %d", y));
         }
     }
 
@@ -28,5 +30,19 @@ public class Point {
 
     public int getY() {
         return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x &&
+                y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
