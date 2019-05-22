a98e42da8537f8ed3753d326ab893ea9097dea98
imkimheejoo <jujudamdadi@naver.com>
2019-05-22 15:30:59 +0900

    [수정] 중복좌표 찾는 메서드 변경

--- src/main/java/coordinate/domain/Coordinate.java
+++ src/main/java/coordinate/domain/Coordinate.java
@@ -1,15 +1,32 @@
 package coordinate.domain;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Coordinate {
-    private List<Point> points;
+    private final X x;
+    private final Y y;
 
-    public Coordinate(List<Point> points) {
-        this.points = points;
+    public Coordinate(X x, Y y) {
+        this.x = x;
+        this.y = y;
     }
 
-    public Point getPoint(int index) {
-        return points.get(index);
+    public double findLength(Coordinate coordinate) {
+        return Math.sqrt(Math.pow(x.sub_Point(coordinate.x), 2) + Math.pow(y.sub_Point(coordinate.y), 2));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Coordinate that = (Coordinate) o;
+        return Objects.equals(x, that.x) &&
+                Objects.equals(y, that.y);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
--- src/test/java/coordinate/domain/CoordinateListTest.java
+++ src/test/java/coordinate/domain/CoordinateListTest.java
@@ -10,12 +10,8 @@ public class CoordinateListTest {
 
     @Test
     void 중복좌표테스트() {
-
-        assertThatIllegalArgumentException().isThrownBy(() -> {
-            new CoordinateList(Arrays.asList(new Coordinate(new X("0"), new Y("0")),
-                    new Coordinate(new X("0"), new Y("0")),
-                    new Coordinate(new X("0"), new Y("0")),
-                    new Coordinate(new X("0"), new Y("0"))));
+        assertThatIllegalArgumentException().isThrownBy(() ->{
+            CoordinateList cl = new CoordinateList(CoordinateFactory.generateCoordinateList(Arrays.asList("0,0","0,0","7,5","10,0")));
         });
     }
 }
