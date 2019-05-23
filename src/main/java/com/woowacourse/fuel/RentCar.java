8400346020d6d35ad521a7a23479c44a1da31704
Jinwoo Shin <bluesky0255@gmail.com>
2019-05-23 10:37:04 +0900

    Add equals() to RentCar

--- src/main/java/com/woowacourse/fuel/RentCar.java
+++ src/main/java/com/woowacourse/fuel/RentCar.java
@@ -1,5 +1,7 @@
 package com.woowacourse.fuel;
 
+import java.util.Objects;
+
 public abstract class RentCar implements Car{
 	private final String name;
 	private final double distancePerLiter;
@@ -20,4 +22,19 @@ public abstract class RentCar implements Car{
 	public double getChargeQuantity() {
 		return distance / distancePerLiter;
 	}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RentCar rentCar = (RentCar) o;
+        return Double.compare(rentCar.distancePerLiter, distancePerLiter) == 0 &&
+            distance == rentCar.distance &&
+            Objects.equals(name, rentCar.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, distancePerLiter, distance);
+    }
 }
