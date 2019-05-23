cf3375b8bd01361ade77a107a272c20c52aee1a4
Jinwoo Shin <bluesky0255@gmail.com>
2019-05-23 10:41:29 +0900

    Refactor main

--- src/main/java/com/woowacourse/coordinate/AppMain.java
+++ src/main/java/com/woowacourse/coordinate/AppMain.java
@@ -7,8 +7,6 @@ import com.woowacourse.coordinate.view.InputView;
 import com.woowacourse.coordinate.view.OutputView;
 
 public class AppMain {
-	private static List<Point> points;
-
 	public static void main(String[] args) {
 		boolean handledResult = tryHandlePoints();
 
@@ -17,7 +15,7 @@ public class AppMain {
 		}
 	}
 
-	public static boolean tryHandlePoints() {
+	private static boolean tryHandlePoints() {
 		try {
 			printPoints();
 			return true;
@@ -27,18 +25,16 @@ public class AppMain {
 		return false;
 	}
 
-	private static boolean printPoints() {
-		points = Point.createWithPair(InputView.inputCoordinate());
+	private static void printPoints() {
+        List<Point> points = Point.createWithPair(InputView.inputCoordinate());
 		FigureCreator figureCreator = new FigureFactory();
 
 		if (points.size() != 1) {
 			Figure figure = figureCreator.create(points);
 			OutputView.printCoordinate(points);
 			OutputView.printShape(figure);
-			return true;
 		}
 
 		OutputView.printCoordinate(points);
-		return true;
 	}
 }
