0782226d4ba9348cd883201aa582f587f2cc7ca0
imkimheejoo <jujudamdadi@naver.com>
2019-05-23 16:49:07 +0900

    [추가] 좌표계산기 Controller 클래스 구현

---
+++ src/main/java/CoordinateCalculator.java
@@ -0,0 +1,21 @@
+import coordinate.domain.PointList;
+import coordinate.domain.Figure;
+import coordinate.domain.FigureFactory;
+import coordinate.domain.view.InputView;
+import coordinate.domain.view.OutputView;
+
+public class CoordinateCalculator {
+    public static void main(String[] args) {
+        PointList pointList = InputView.InputPoints();
+        OutputView.printPoints(pointList);
+        Figure figure;
+        try {
+            figure = FigureFactory.generateFigure(pointList);
+            OutputView.printResult(figure);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+
+    }
+}
