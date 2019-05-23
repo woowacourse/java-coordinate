997fcbdf81ad21456b58c7d08abec6a6e4ca10f0
dasistHYOJIN <hgenie14@gmail.com>
2019-05-23 14:41:08 +0900

    refactor: remove multiple called method

--- src/main/java/coordinatecalculator/util/PointsGenerator.java
+++ src/main/java/coordinatecalculator/util/PointsGenerator.java
@@ -3,21 +3,18 @@ package coordinatecalculator.util;
 import coordinatecalculator.domain.Point;
 import coordinatecalculator.domain.Points;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class PointsGenerator {
-
-    public static final String DUPLICATED_POINT_MESSAGE = "중복되는 점이 존재합니다. 다시 입력해주세요.";
+    private static final String DUPLICATED_POINT_MESSAGE = "중복되는 점이 존재합니다. 다시 입력해주세요.";
 
     public static Points create(String input) {
         String[] inputs = input.split("-");
         List<Point> points = new ArrayList<>();
-        //TODO: refactoring
+
         for (String s : inputs) {
-            points.add(new Point(s.split("\\(|\\)|,")[1], s.split("\\(|\\)|,")[2]));
+            List<String> splittedPoint = Arrays.asList(s.split("\\(|\\)|,"));
+            points.add(new Point(splittedPoint.get(1), splittedPoint.get(2)));
         }
 
         checkDuplicatedPoint(points);
--- src/main/java/coordinatecalculator/view/InputView.java
+++ src/main/java/coordinatecalculator/view/InputView.java
@@ -8,8 +8,6 @@ public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static Figure inputLocation() {
-        // TODO: enum으로 Line, Tri, Rec 구분
-
         return null;
     }
 }
