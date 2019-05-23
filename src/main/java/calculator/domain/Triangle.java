fb3008171a3d78fa82a646e6b212ef96c6a734af
heebong <heebg2@naver.com>
2019-05-23 16:31:21 +0900

    feat Triangle class 구현

---
+++ src/main/java/calculator/domain/Triangle.java
@@ -0,0 +1,40 @@
+package calculator.domain;
+
+import java.util.Objects;
+
+/**
+ * @author heebg
+ * @version 1.0 2019-05-23
+ */
+public class Triangle extends Figure {
+
+    private final Coordinates coordinates;
+
+    public Triangle(Coordinates coordinates) {
+        super("삼각형", "넓이");
+        this.coordinates = coordinates;
+    }
+
+    @Override
+    public double area() {
+        double powerLengthA = Math.pow(straight(coordinates.get(0), coordinates.get(1)), 2);
+        double powerLengthB = Math.pow(straight(coordinates.get(1), coordinates.get(2)), 2);
+        double powerLengthC = Math.pow(straight(coordinates.get(0), coordinates.get(2)), 2);
+        double triangleArea = Math.sqrt(4 * powerLengthA * powerLengthB - Math.pow(powerLengthA + powerLengthB - powerLengthC, 2)) / 4;
+        return triangleArea;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Triangle triangle = (Triangle) o;
+        return Objects.equals(coordinates, triangle.coordinates);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(coordinates);
+    }
+
+}
---
+++ src/test/java/calculator/domain/TriangleTest.java
@@ -0,0 +1,36 @@
+package calculator.domain;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.offset;
+
+/**
+ * @author heebg
+ * @version 1.0 2019-05-23
+ */
+public class TriangleTest {
+
+    Figure triangle;
+    Coordinates coordinates;
+
+    @BeforeEach
+    void setUp() {
+        coordinates = new Coordinates();
+        coordinates.add(Coordinate.create(0 ,0));
+        coordinates.add(Coordinate.create(10 ,0));
+        coordinates.add(Coordinate.create(5 ,10));
+        triangle = FigureFactory.getInstance().create(coordinates);
+    }
+
+    @Test
+    void create_생성_확인() {
+        assertThat(triangle).isEqualTo(FigureFactory.getInstance().create(coordinates));
+    }
+
+    @Test
+    void area_삼각형_넓이_확인() {
+        assertThat(triangle.area()).isEqualTo(50.000,offset(0.00099));
+    }
+}
