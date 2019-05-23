45b9a8dd09e40dfc1699206841ab32e4e02147ac
hyojaekim <hj00686@naver.com>
2019-05-23 15:18:44 +0900

    feat(Point) : create Point adding a method to sum of vector, dot of vector, and diagonal points

--- src/main/java/coordinate/domain/Point.java
+++ src/main/java/coordinate/domain/Point.java
@@ -2,7 +2,7 @@ package coordinate.domain;
 
 import java.util.Objects;
 
-public class Point {
+public class Point implements Comparable<Point>{
     private final int x;
     private final int y;
 
@@ -13,6 +13,24 @@ public class Point {
 
     public double sub(Point secondPoint) {
         return Math.sqrt(Math.pow(x - secondPoint.x, 2) + Math.pow(y - secondPoint.y, 2));
+    }
+
+
+    public Point calVector(Point point) {
+        return new Point(point.x - x, point.y - y);
+    }
+
+    public int calDotProduct(Point vector) {
+        return (vector.x * x) + (vector.y * y);
+    }
+
+    public Point sumOfVector(Point vector) {
+        return new Point(this.x + vector.x, this.y + vector.y);
+    }
+
+    public Point calCrossPoint(Point firstVector, Point secondVector) {
+        Point addedVector = firstVector.sumOfVector(secondVector);
+        return new Point(x + addedVector.x, y + addedVector.y);
     }
 
     @Override
@@ -27,5 +45,21 @@ public class Point {
     @Override
     public int hashCode() {
         return Objects.hash(x, y);
+    }
+
+    @Override
+    public int compareTo(Point o) {
+        if (this.y == o.y) {
+            return this.x - o.x;
+        }
+        return this.y - o.y;
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
     }
 }
