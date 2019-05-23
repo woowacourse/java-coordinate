922d390ebc8b5bd323e1b57c08dc5613e74eeeac
starkim06 <starkim06@naver.com>
2019-05-23 14:41:50 +0900

    refactor: OutputView.class 상수생성 및 함수분리

--- src/main/java/coordinate/view/OutputView.java
+++ src/main/java/coordinate/view/OutputView.java
@@ -6,41 +6,72 @@ import coordinate.domain.Points;
 
 //(10,10)-(22,10)-(22,18)-(10,18)
 public class OutputView {
+    private static final int MAX_X = 25;
+    private static final int MAX_Y = 25;
+    private static final String EXIST_POINT = " . ";
+    private static final String EMPTY_POINT = "   ";
+    private static final String EMPTY_LINE = "\n";
+    private static final String X_AXIS_NUMBER = "0     2      4     6      8     10    12    14    16    18    20    22    24";
+    private static final String Y_AXIS_LINE = "|";
+    private static final String ORIGIN_POINT = "+";
+    private static final String X_AXIS_LINE = "---";
+
+
     public static void printCoordinate(Points points) {
-        boolean[][] coorinates = new boolean[25][25];
-        for (int i = 0; i < 25; i++) {
-            for (int j = 0; j < 25; j++) {
-                coorinates[i][j] = false;
+        boolean[][] coordinates = new boolean[MAX_Y][MAX_X];
+        initCoordinates(coordinates);
+        drawPoints(points, coordinates);
+        printPoints(coordinates);
+        printXAxis();
+    }
+
+    private static void initCoordinates(boolean[][] coordinates) {
+        for (int i = 0; i < MAX_Y; i++) {
+            for (int j = 0; j < MAX_X; j++) {
+                coordinates[i][j] = false;
             }
         }
+    }
+
+    private static void drawPoints(Points points, boolean[][] coordinates) {
         for (Point point : points.getPoints()) {
-            coorinates[point.getY()][point.getX()] = true;
+            coordinates[point.getY()][point.getX()] = true;
         }
-        for (int i = 24; i >= 0; i--) {
-            if (i % 2 == 0) {
-                System.out.printf("%3s", i + "|");
-            } else {
-                System.out.printf("%3s", "|");
-            }
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < 25; j++) {
-                if (coorinates[i][j]) {
-                    sb.append(" . ");
+    }
+
+    private static void printPoints(boolean[][] coordinates) {
+        for (int i = MAX_Y - 1; i >= 0; i--) {
+            StringBuilder sb = initStringBuilder(i);
+            for (int j = 0; j < MAX_X; j++) {
+                if (coordinates[i][j]) {
+                    sb.append(EXIST_POINT);
                     continue;
                 }
-                sb.append("   ");
+                sb.append(EMPTY_POINT);
             }
             System.out.println(sb.toString());
         }
+    }
 
+    private static StringBuilder initStringBuilder(int index) {
+        StringBuilder sb = new StringBuilder();
+        if (index % 2 == 0) {
+            sb.append(String.format("%3s", index + Y_AXIS_LINE));
+            return sb;
+        }
+        return sb.append(String.format("%3s", Y_AXIS_LINE));
+    }
+
+    private static void printXAxis() {
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < 24; i++) {
             if (i == 0) {
-                System.out.printf("%3s", "+");
+                sb.append(String.format("%3s", ORIGIN_POINT));
             }
-            System.out.print("---");
+            sb.append(X_AXIS_LINE);
         }
-        System.out.println();
-        System.out.println("0     2      4     6      8     10    12    14    16    18    20    22    24");
+        sb.append(EMPTY_LINE + X_AXIS_NUMBER);
+        System.out.println(sb.toString());
     }
 
     public static void printArea(Figure figure) {
