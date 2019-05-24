86f1720f3a5037be21a5cbea50dee4cac2c8c00f
김성훈 <seonghun127@gmail.com>
2019-05-24 18:53:11 +0900

    refactor : 코드 포맷팅

--- src/test/java/coordinate/domain/LineTest.java
+++ src/test/java/coordinate/domain/LineTest.java
@@ -21,7 +21,8 @@ public class LineTest {
     public void 두_점_사이의_거리를_제대로_출력해주는지_테스트() {
         List<Point> points = Arrays.asList(
                 new Point(10, 10),
-                new Point(14, 15));
+                new Point(14, 15)
+        );
 
         Shape line = new Line(points);
         assertThat(line.area()).isEqualTo(6.403124, offset(0.00099));
@@ -31,7 +32,8 @@ public class LineTest {
     public void 두_점이_같은_위치일_때_예외처리_테스트() {
         List<Point> points = Arrays.asList(
                 new Point(10, 10),
-                new Point(10, 10));
+                new Point(10, 10)
+        );
 
         assertThrows(IllegalArgumentException.class, () -> {
             new Line(points);
--- src/test/java/coordinate/domain/PointsGeneratorTest.java
+++ src/test/java/coordinate/domain/PointsGeneratorTest.java
@@ -9,11 +9,11 @@ public class PointsGeneratorTest {
     public void 문자열_패턴이_일치하지않으면_예외처리_테스트() {
         //assertThrows(IllegalArgumentException.class, () -> new ShapeGenerator("(10,11)-(19,20"));
         //assertThrows(IllegalArgumentException.class, () -> new ShapeGenerator("(10,11)(19,20)"));
-        assertThrows(IllegalArgumentException.class, () -> new ShapeGenerator().create(PointsGenerator.generatePoints("(a,11)-(19,20)")));
+        assertThrows(IllegalArgumentException.class, () -> ShapeGenerator.create(PointsGenerator.generatePoints("(a,11)-(19,20)")));
     }
 
     @Test
     public void 좌표의_개수가_범위를_벗어났을_경우_예외처리_테스트() {
-        assertThrows(IllegalArgumentException.class, () -> new ShapeGenerator().create(PointsGenerator.generatePoints("(1,11)")));
+        assertThrows(IllegalArgumentException.class, () -> ShapeGenerator.create(PointsGenerator.generatePoints("(1,11)")));
     }
 }
