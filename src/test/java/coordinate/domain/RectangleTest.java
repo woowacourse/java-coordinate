b81e689a8d3e60e0c9340a6acca85c628c408b90
ep1stas1s <masloz@konkuk.ac.kr>
2019-05-24 12:00:04 +0900

    refactor: test case 추가

--- src/test/java/coordinate/domain/RectangleTest.java
+++ src/test/java/coordinate/domain/RectangleTest.java
@@ -5,13 +5,27 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RectangleTest {
-
     @Test
     void 사각형_넓이_구하기() {
-        Points points = new Points(Arrays.asList(new Point(1, 5), new Point(1, 1), new Point(5, 1), new Point(5, 5)));
+        Points points = new Points(Arrays.asList(
+                new Point(1, 5),
+                new Point(1, 1),
+                new Point(5, 1),
+                new Point(5, 5)));
         Rectangle rectangle = new Rectangle(points);
         assertThat(rectangle.calculateResult()).isEqualTo(16);
+    }
+
+    @Test
+    void 사각형_생성() {
+        Points points = new Points(Arrays.asList(
+                new Point(1, 5),
+                new Point(1, 1),
+                new Point(5, 1),
+                new Point(5, 4)));
+        assertThatThrownBy(() -> new Rectangle(points)).isInstanceOf(IllegalArgumentException.class);
     }
 }
