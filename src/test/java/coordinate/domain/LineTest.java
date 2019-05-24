684a0030ac8cbc2d15282313eecf381e8cf5c0c6
jinwook <jin942002@naver.com>
2019-05-24 23:22:08 +0900

    refactor: LineTest에서 테스트 케이스 삭제.

--- src/test/java/coordinate/domain/LineTest.java
+++ src/test/java/coordinate/domain/LineTest.java
@@ -1,36 +1,17 @@
 package coordinate.domain;
 
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import coordinate.domain.figure.Line;
+import coordinate.domain.nonefigure.Point;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class LineTest {
-//    @Test
-//    void 중복된_점_입력() {
-//        assertThrows(IllegalArgumentException.class, () -> {
-//            new Line(Point.create(1, 1), Point.create(1, 1));
-//        });
-//    }
-//
-//    @Test
-//    void 라인_길이() {
-//        Point point1 = Point.create(6, 3);
-//        Point point2 = Point.create(9, 7);
-//        Line line = new Line(point1, point2);
-//        assertEquals(5.0, line.length(), 0.1);
-//    }
-//
-//    @Test
-//    void 직교라인_성공() {
-//        Line line1 = new Line(Point.create(1, 3), Point.create(2, 4));
-//        Line line2 = new Line(Point.create(2, 4), Point.create(3, 3));
-//        assertThat(line1.orthogonalTo(line2)).isTrue();
-//    }
-//
-//    @Test
-//    void 직교라인_실패() {
-//        Line line1 = new Line(Point.create(1, 3), Point.create(2, 4));
-//        Line line2 = new Line(Point.create(2, 4), Point.create(2, 2));
-//        assertThat(line1.orthogonalTo(line2)).isFalse();
-//    }
+    @Test
+    void 라인_길이() {
+        Point point1 = Point.create(6, 3);
+        Point point2 = Point.create(9, 7);
+        Line line = new Line(point1, point2);
+        assertEquals(5.0, line.length(), 0.1);
+    }
 }
