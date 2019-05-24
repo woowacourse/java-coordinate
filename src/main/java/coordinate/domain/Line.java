590d0da93e974bf2a9ccd5e6d781ac1b8e15fa35
hyojaekim <hj00686@naver.com>
2019-05-24 17:46:01 +0900

    feat(Line): update Line constructor

--- src/main/java/coordinate/domain/Line.java
+++ src/main/java/coordinate/domain/Line.java
@@ -1,15 +1,15 @@
 package coordinate.domain;
 
-public class Line {
-    private final Point firstPoint;
-    private final Point secondPoint;
+public class Line extends Figure {
 
-    public Line(Point firstPoint, Point secondPoint) {
-        this.firstPoint = firstPoint;
-        this.secondPoint = secondPoint;
+    public Line(Points points) {
+        super(points);
+        if (points.size() != 2) {
+            throw new IllegalArgumentException("2개의 점으로 구성되어야 합니다");
+        }
     }
 
-    public double calLength() {
-        return firstPoint.sub(secondPoint);
+    public double calculateFigure() {
+        return points.get(0).sub(points.get(1));
     }
 }
--- src/test/java/coordinate/domain/LineTest.java
+++ src/test/java/coordinate/domain/LineTest.java
@@ -2,12 +2,14 @@ package coordinate.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LineTest {
     @Test
     void 선길이_구하기_테스트() {
-        Line line = new Line(new Point(1, 2), new Point(1, 5));
-        assertThat(line.calLength()).isEqualTo(3);
+        Line line = new Line(new Points(Arrays.asList(new Point(1, 2), new Point(1, 5))));
+        assertThat(line.calculateFigure()).isEqualTo(3);
     }
 }
