ee1b6f466045246a1dca06d4c71fcbe1fa784e2f
hyojaekim <hj00686@naver.com>
2019-05-24 17:50:52 +0900

    feat(CoordinateController): create CoordinateController

--- README.md
+++ README.md
@@ -38,6 +38,7 @@
 * Figure 상속 적용하기
 * Shape Interface 적용하기
 * Shape Factory 적용하기
+* CoordinateController 만들기
 
 ## 우아한테크코스 코드리뷰
 * [온라인 코드 리뷰 과정](https://github.com/woowacourse/woowacourse-docs/blob/master/maincourse/README.md)
---
+++ src/main/java/coordinate/controller/CoordinateController.java
@@ -0,0 +1,16 @@
+package coordinate.controller;
+
+import coordinate.domain.Board;
+import coordinate.domain.Points;
+import coordinate.domain.Shape;
+import coordinate.domain.ShapeFactory;
+import coordinate.dto.CoordinateDto;
+
+public class CoordinateController {
+    public static CoordinateDto request(Points points) {
+        Shape shape = ShapeFactory.create(points);
+        Board board = new Board(points);
+
+        return new CoordinateDto(board, shape);
+    }
+}
