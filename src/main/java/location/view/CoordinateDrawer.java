3fc7107bb70d6cdc8cf1d15805bf2ed8bebae7c0
kmdngyu <kmdngyu@gmail.com>
2019-05-24 17:11:58 +0900

    refactor 그래프 그리기 0 나오게

--- src/main/java/location/view/CoordinateDrawer.java
+++ src/main/java/location/view/CoordinateDrawer.java
@@ -15,46 +15,50 @@ public class CoordinateDrawer {
 
     void drawMap(Points points) {
         for (int i = 0; i < MAX_HEIGHT_WIDTH; i++) {
-            drawVertical(MAX_HEIGHT_WIDTH - i);
-            drawDot(MAX_HEIGHT_WIDTH - i, points);
+            drawVertical(MAX_HEIGHT_WIDTH - i, points);
             System.out.println();
         }
         drawHorizontal(MAX_HEIGHT_WIDTH);
     }
 
-    void drawVertical(int currentY) {
+    private void drawVertical(int currentY, Points points) {
         String numOrBlank = "  ";
         if (currentY % 2 == 0) {
             numOrBlank = String.format("%2s", Integer.toString(currentY));
         }
-        System.out.print(numOrBlank + "| ");
-    }
-
-    private void drawDot(int currentY, Points points) {
-        List<Integer> currentXGroup = points.getPoints().stream().filter(x -> x.getY() == currentY).map(e -> e.getX()).collect(Collectors.toList());
-        for (int currentX : currentXGroup) {
-            drawDotXs((currentX - 1) * HorizontalCorrectionValue/2);
+        List<Integer> currentXGroup = points.getPoints().stream().filter(x -> x.getY() == currentY && x.getY()>0).map(e -> e.getX()).collect(Collectors.toList());
+        if (currentXGroup.size() > 0 && currentXGroup.get(0) == 0) {
+            System.out.print(numOrBlank + "●");
+            currentXGroup.remove(0);
+        } else {
+            System.out.print(numOrBlank + "|");
         }
+        drawDotX(currentXGroup);
     }
 
-    private void drawDotXs(int currentX) {
+
+    private void drawDotX(List<Integer> currentXGroup) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < currentX; i++) {
-            sb.append(" ");
+        int beforeX =0 ;
+        for (int currentX : currentXGroup) {
+            for (int i = 0; i < ((currentX-beforeX)*(HorizontalCorrectionValue)/2)-1; i++) {
+                sb.append(" ");
+            }
+            sb.append("●");
+            beforeX = currentX;
         }
-        sb.append("●");
         System.out.print(sb.toString());
     }
 
 
     void drawHorizontal(int num) {
         String firstLine = "  +";
-        for (int i = 0; i < num*HorizontalCorrectionValue/2; i++) {
-            firstLine += "-";
+        for (int i = 0; i < num * HorizontalCorrectionValue / 2; i++) {
+            firstLine += "—";
         }
-        String secondLine = " 0 ";
+        String secondLine = "  0";
         for (int i = 0; i < num / 2; i++) {
-            secondLine += String.format("%"+HorizontalCorrectionValue+"s", Integer.toString((i + 1) * 2));
+            secondLine += String.format("%" + HorizontalCorrectionValue + "s", Integer.toString((i + 1) * 2));
         }
         System.out.println(firstLine);
         System.out.println(secondLine);
