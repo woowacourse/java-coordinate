cba20f46cb4af7edbe84ba149ccc77fc1b1d625d
huisam <huisam@naver.com>
2019-05-24 18:14:42 +0900

    refactor : create 메서드 리팩토링
    return null 보다는 abstract 키워드로 오버라이딩 형태로 구현

--- src/main/java/coordinatecalculator/factory/ShapeType.java
+++ src/main/java/coordinatecalculator/factory/ShapeType.java
@@ -7,16 +7,19 @@ import java.util.List;
 
 enum ShapeType {
     LINE(ShapeMaker.COORDINATES_FOR_LINE) {
+        @Override
         public Shape create(List<Coordinate> coordinates) {
             return new Line(coordinates);
         }
     },
     TRIANGLE(ShapeMaker.COORDINATES_FOR_TRIANGLE) {
+        @Override
         public Shape create(List<Coordinate> coordinates) {
             return new Triangle(coordinates);
         }
     },
     RECTANGLE(ShapeMaker.COORDINATES_FOR_RECTANGLE) {
+        @Override
         public Shape create(List<Coordinate> coordinates) {
             return new Rectangle(coordinates);
         }
@@ -28,9 +31,7 @@ enum ShapeType {
         this.countOfCoordinate = countOfCoordinate;
     }
 
-    public Shape create(final List<Coordinate> coordinates) {
-        return null;
-    }
+    public abstract Shape create(final List<Coordinate> coordinates);
 
     public static ShapeType valueOf(final int countOfCoordinate) {
         return Arrays.stream(values())
