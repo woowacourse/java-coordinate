f98b8aa973d534d394c4b6d3d9b13c44c45c768f
mrkwon <erased11@gmail.com>
2019-05-24 15:29:06 +0900

    ADD : RentCompany Class

---
+++ src/main/java/com/woowa/rentcar/RentCompany.java
@@ -0,0 +1,30 @@
+package com.woowa.rentcar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RentCompany {
+    private static final String NEWLINE = System.getProperty("line.separator");
+
+    private List<Car> list;
+
+    private RentCompany() {
+        list = new ArrayList<>();
+    }
+
+    public static RentCompany create() {
+        return new RentCompany();
+    }
+
+    public void addCar(Car car) {
+        list.add(car);
+    }
+
+    public String generateReport() {
+        StringBuilder sb = new StringBuilder();
+        for (Car aCar : list) {
+            sb.append(aCar.getName()).append(" : ").append(aCar.getFuel()).append("리터").append(NEWLINE);
+        }
+        return sb.toString();
+    }
+}
