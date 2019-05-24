14e3c90c479273259623004335d219cc31b9e1e6
mrkwon <erased11@gmail.com>
2019-05-24 17:30:12 +0900

    ADD : Main Class

---
+++ src/main/java/com/woowa/rentcar/Main.java
@@ -0,0 +1,18 @@
+package com.woowa.rentcar;
+
+import com.woowa.rentcar.domain.Car;
+import com.woowa.rentcar.domain.CarFactory;
+import com.woowa.rentcar.domain.RentCompany;
+import com.woowa.rentcar.view.InputView;
+
+public class Main {
+    public static void main(String[] args){
+        RentCompany rentCompany = RentCompany.create();
+        InputView.title();
+        String carName = InputView.car();
+        int distance = InputView.distance();
+        Car car = CarFactory.newInstance(carName, distance);
+        rentCompany.addCar(car);
+        System.out.println(rentCompany.generateReport());
+    }
+}
