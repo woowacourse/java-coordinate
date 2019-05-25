300fd6fc09ffb5a729d593cdbd89881b5686ae34
DaejunBae <dpudpu11@gmail.com>
2019-05-25 16:57:24 +0900

    refactor: RealFigure.class 생성자 접근제어자 수정

--- src/main/java/coordinate/domain/RealFigure.java
+++ src/main/java/coordinate/domain/RealFigure.java
@@ -5,7 +5,7 @@ import coordinate.domain.generator.LinesGenerator;
 public abstract class RealFigure extends AbstractFigure {
     private Lines lines;
 
-    public RealFigure(final Points points, final LinesGenerator linesGenerator) {
+    RealFigure(final Points points, final LinesGenerator linesGenerator) {
         super(points);
         this.lines = linesGenerator.generate();
     }
