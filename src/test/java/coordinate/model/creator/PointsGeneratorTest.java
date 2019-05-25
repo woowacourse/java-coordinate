3d8df2479a62c5678f05f5806db832f17a37f753
hyun <jhs695@naver.com>
2019-05-26 01:35:23 +0900

    fix 생성자 인자 변경으로 test 코드 수정

---
+++ src/test/java/coordinate/model/creator/PointsGeneratorTest.java
@@ -0,0 +1,27 @@
+package coordinate.model.creator;
+
+import coordinate.model.Point;
+import coordinate.model.Value;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class PointsGeneratorTest {
+
+    @Test
+    void 포인트_생성_테스트() {
+        List<Point> testPoints = Arrays.asList(new Point(new Value(1), new Value(2)),
+                new Point(new Value(3), new Value(4)));
+        AssertionsForClassTypes.assertThat(PointsGenerator.makePoints("(1,2)-(3,4)")).isEqualTo(testPoints);
+    }
+    @Test
+    void 포인트_중복_테스트() {
+        assertThrows(IllegalArgumentException.class,
+                ()-> PointsGenerator.makePoints("(1,1)-(1,1)"));
+    }
+
+}
