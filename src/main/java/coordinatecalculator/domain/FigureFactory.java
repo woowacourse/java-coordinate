4687eda682400e6724eec96777928796861292c1
강연욱 <fiasboc23@gmail.com>
2019-05-26 00:20:28 +0900

    feat : Figure 생성

---
+++ src/main/java/coordinatecalculator/domain/FigureFactory.java
@@ -0,0 +1,31 @@
+package coordinatecalculator.domain;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+public class FigureFactory implements FigureCreator {
+    private static final int LINE_SIZE = 2;
+    private static final int TRIANGLE_SIZE = 3;
+    private static final int RECTANGLE_SIZE = 4;
+    private static final String INSUFFICIENT_POINTS = "입력한 좌표의 개수는 '선', '삼각형', '직사각형'을 만들 수 없습니다.";
+
+    private Map<Integer, Function<List<Point>, Area>> figures;
+
+    public FigureFactory(){
+        figures  = new HashMap<>();
+        figures.put(LINE_SIZE, Line::new);
+        figures.put(TRIANGLE_SIZE, Triangle::new);
+        figures.put(RECTANGLE_SIZE, Rectangle::new);
+    }
+
+    @Override
+    public Area create(List<Point> points) {
+        try {
+            return figures.get(points.size()).apply(points);
+        } catch (NullPointerException npe) {
+            throw new IllegalArgumentException(INSUFFICIENT_POINTS);
+        }
+    }
+}
