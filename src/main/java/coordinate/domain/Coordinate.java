20534823c9efac5ffd446f11651513edf9d0a61b
DaejunBae <dpudpu11@gmail.com>
2019-05-25 12:31:29 +0900

    refactor: Coordinate.class 미리 인스턴스 생성 해놓고 반환되게 변경

--- src/main/java/coordinate/domain/Coordinate.java
+++ src/main/java/coordinate/domain/Coordinate.java
@@ -1,22 +1,36 @@
 package coordinate.domain;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class Coordinate {
     private static final int MAX_BOUND = 24;
     private static final int MIN_BOUND = 0;
 
+    private static Map<Integer, Coordinate> map = new HashMap<>();
+
+    static {
+        for (int i = MIN_BOUND; i <= MAX_BOUND; i++) {
+            map.put(i, of(i));
+        }
+    }
+
     private final int coordinate;
 
     private Coordinate(final int coordinate) {
         if (coordinate < MIN_BOUND || coordinate > MAX_BOUND) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("좌표는 0~24만 입력 가능합니다.");
         }
         this.coordinate = coordinate;
     }
 
     public static Coordinate of(final int coordinate) {
-        return new Coordinate(coordinate);
+        Coordinate instance = map.get(coordinate);
+        if(map.get(coordinate)==null) {
+            return new Coordinate(coordinate);
+        }
+        return instance;
     }
 
     public int value() {
