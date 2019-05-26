e302d0997003fc982380eede9533324f4fa917e4
Soojin Roh <sooreal91@gmail.com>
2019-05-26 22:07:44 +0900

    refactor 중복 모드 변수화하여 제거
    삼각형 각 변의 길이를 구하는 코드가 중복되어 있던 것을 인스턴스 변수로 만들어 중복된 코드를 제거함

--- src/main/java/calculator/domain/Triangle.java
+++ src/main/java/calculator/domain/Triangle.java
@@ -12,38 +12,41 @@ import java.util.Objects;
 public class Triangle extends Figure {
 
     private static final String EX_NOT_TRIANGLE_MESSAGE = "삼각형이 될 수 없는 조건입니다.";
-    private static final int TRI_LONGEST_SIDE = 2;
+    private static final int LONGEST_SIDE_OF_TRIANGLE = 2;
     private static final int TRI_OTHER_SIDE = 1;
     private static final int TRI_ANOTHER_SIDE = 0;
     private final Coordinates coordinates;
+    private final double ASideOfTriangle;
+    private final double BSideOfTriangle;
+    private final double CSideOfTriangle;
 
     public Triangle(Coordinates coordinates) {
         super("삼각형", "넓이");
         this.coordinates = coordinates;
         checkTriangle();
+
+        ASideOfTriangle = straight(coordinates.get(0), coordinates.get(1));
+        BSideOfTriangle = straight(coordinates.get(1), coordinates.get(2));
+        CSideOfTriangle = straight(coordinates.get(0), coordinates.get(2));
     }
 
     private void checkTriangle() {
-        double lengthA = straight(coordinates.get(0), coordinates.get(1));
-        double lengthB = straight(coordinates.get(1), coordinates.get(2));
-        double lengthC = straight(coordinates.get(0), coordinates.get(2));
-
-        List<Double> lengths = Arrays.asList(lengthA, lengthB, lengthC);
+        List<Double> lengths = Arrays.asList(ASideOfTriangle, BSideOfTriangle, CSideOfTriangle);
         Collections.sort(lengths);
         checkNotTriangle(lengths);
     }
 
     private void checkNotTriangle(List<Double> lengths) {
-        if (lengths.get(TRI_LONGEST_SIDE) >= lengths.get(TRI_OTHER_SIDE) + lengths.get(TRI_ANOTHER_SIDE)) {
+        if (lengths.get(LONGEST_SIDE_OF_TRIANGLE) >= lengths.get(TRI_OTHER_SIDE) + lengths.get(TRI_ANOTHER_SIDE)) {
             throw new IllegalArgumentException(EX_NOT_TRIANGLE_MESSAGE);
         }
     }
 
     @Override
     public double area() {
-        double powerLengthA = Math.pow(straight(coordinates.get(0), coordinates.get(1)), 2);
-        double powerLengthB = Math.pow(straight(coordinates.get(1), coordinates.get(2)), 2);
-        double powerLengthC = Math.pow(straight(coordinates.get(0), coordinates.get(2)), 2);
+        double powerLengthA = Math.pow(ASideOfTriangle, 2);
+        double powerLengthB = Math.pow(BSideOfTriangle, 2);
+        double powerLengthC = Math.pow(CSideOfTriangle, 2);
         double triangleArea = Math.sqrt(4 * powerLengthA * powerLengthB - Math.pow(powerLengthA + powerLengthB - powerLengthC, 2)) / 4;
         return triangleArea;
     }
