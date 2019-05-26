0713a0923228305161b312a6e39ed6ff92d24153
wbluke <pwb0127@naver.com>
2019-05-27 00:50:02 +0900

    [REF] FigureFactory 에 Figure를 Point개수에 따라 분기해서 생성해주는 classifier map 로직 추가

--- src/main/java/coordinate/model/FigureFactory.java
+++ src/main/java/coordinate/model/FigureFactory.java
@@ -1,34 +1,39 @@
 package coordinate.model;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 public class FigureFactory {
     private static final String ERROR_INVALID_FIGURE_CREATION = "입력된 Point 개수가 유효하지 않습니다.";
-    private static final int ONE_POINT = 1;
     private static final int NUM_OF_VERTICES_OF_LINE = 2;
     private static final int NUM_OF_VERTICES_OF_TRIANGLE = 3;
     private static final int NUM_OF_VERTICES_OF_RECTANGLE = 4;
+    private static final Map<Integer, Function<List<Point>, Figure>> classifier = new HashMap<>();
+
+    static {
+        classifier.put(NUM_OF_VERTICES_OF_LINE, Line::new);
+        classifier.put(NUM_OF_VERTICES_OF_TRIANGLE, Triangle::new);
+        classifier.put(NUM_OF_VERTICES_OF_RECTANGLE, Rectangle::new);
+    }
 
     public static Figure create(List<Point> points) {
         if (points == null) {
             throw new IllegalArgumentException(AbstractFigure.ERROR_FIGURE_NULL);
         }
-        if (points.size() == ONE_POINT) {
+        if (isInvalidNumberOf(points)) {
             throw new IllegalArgumentException(ERROR_INVALID_FIGURE_CREATION);
         }
         return classifyFigure(points);
     }
 
+    private static boolean isInvalidNumberOf(List<Point> points) {
+        int numOfPoints = points.size();
+        return numOfPoints < NUM_OF_VERTICES_OF_LINE || numOfPoints > NUM_OF_VERTICES_OF_RECTANGLE;
+    }
+
     private static Figure classifyFigure(List<Point> points) {
-        if (points.size() == NUM_OF_VERTICES_OF_LINE) {
-            return new Line(points);
-        }
-        if (points.size() == NUM_OF_VERTICES_OF_TRIANGLE) {
-            return new Triangle(points);
-        }
-        if (points.size() == NUM_OF_VERTICES_OF_RECTANGLE) {
-            return new Rectangle(points);
-        }
-        throw new IllegalArgumentException(ERROR_INVALID_FIGURE_CREATION);
+        return classifier.get(points.size()).apply(points);
     }
 }
