2088202b3952ba6e57a6641ca96ead1817637a94
hyperpace <cnk9109@gmail.com>
2019-05-26 17:08:15 +0900

    feat(Board): update Board adding check validation point range

--- src/main/java/coordinate/domain/Board.java
+++ src/main/java/coordinate/domain/Board.java
@@ -4,25 +4,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Board {
-    private final static int MAX_BOARD_SIZE = 25;
-    private final static int MIN_BOARD_SIZE = 0;
+    final static int MIN_BOARD_SIZE = 0;
+    final static int MAX_BOARD_SIZE = 25;
 
     private final List<List<Boolean>> board;
 
     public Board(Points points) {
+        checkValidPoints(points);
         this.board = initBoard();
         setPoints(points);
+    }
+
+    private void checkValidPoints(Points points) {
+        for (int pointIndex = 0; pointIndex < points.size(); pointIndex++) {
+            checkValidPoint(points.get(pointIndex));
+        }
+    }
+
+    private void checkValidPoint(Point point) {
+        if (point.getX() < MIN_BOARD_SIZE || point.getX() > MAX_BOARD_SIZE) {
+            throw new IllegalArgumentException(String.format("X값은 %d ~ %d 입니다.", MIN_BOARD_SIZE, MAX_BOARD_SIZE));
+        }
+
+        if (point.getY() < MIN_BOARD_SIZE || point.getY() > MAX_BOARD_SIZE) {
+            throw new IllegalArgumentException(String.format("Y값은 %d ~ %d 입니다.", MIN_BOARD_SIZE, MAX_BOARD_SIZE));
+        }
     }
 
     private List<List<Boolean>> initBoard() {
         List<List<Boolean>> board = new ArrayList<>();
         for (int axisY = MIN_BOARD_SIZE; axisY < MAX_BOARD_SIZE; axisY++) {
             board.add(new ArrayList<>());
-            for (int axisX = MIN_BOARD_SIZE; axisX < MAX_BOARD_SIZE; axisX++) {
-                board.get(axisY).add(false);
-            }
+            initAxisX(board, axisY);
         }
         return board;
+    }
+
+    private void initAxisX(List<List<Boolean>> board, int axisY) {
+        for (int axisX = MIN_BOARD_SIZE; axisX < MAX_BOARD_SIZE; axisX++) {
+            board.get(axisY).add(false);
+        }
     }
 
     void setPoints(Points points) {
--- src/test/java/coordinate/domain/BoardTest.java
+++ src/test/java/coordinate/domain/BoardTest.java
@@ -3,14 +3,27 @@ package coordinate.domain;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 public class BoardTest {
     @Test
-    void 보드_생성하는_테스트() {
-        Board board = new Board(new Points(Arrays.asList(new Point(1, 2), new Point(3, 4), new Point(0, 0), new Point(0, 10), new Point(10, 0))));
-        System.out.println(board.toString());
-//        assertThat(board).isEqualTo(new Board(new Points(Arrays.asList(new Point(1, 2), new Point(3, 4)))));
+    void 보드_유효성_X_예외_테스트() {
+        Points points = new Points(Arrays.asList(new Point(-1,3), new Point(3, 5)));
+
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            new Board(points);
+        }).withMessage(String.format("X값은 %d ~ %d 입니다.", Board.MIN_BOARD_SIZE, Board.MAX_BOARD_SIZE));
+    }
+
+    @Test
+    void 보드_유효성_Y_예외_테스트() {
+        Points points = new Points(Arrays.asList(new Point(0,26), new Point(3, 5)));
+
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> {
+            new Board(points);
+        }).withMessage(String.format("Y값은 %d ~ %d 입니다.", Board.MIN_BOARD_SIZE, Board.MAX_BOARD_SIZE));
     }
 }
