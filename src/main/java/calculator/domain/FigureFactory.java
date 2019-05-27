b10248f51ce611dd6332080fa88214c9d2e67e83
Soojin Roh <sooreal91@gmail.com>
2019-05-27 20:28:22 +0900

    refactor Map을 이용해 if문 제거
    - FigureFactory에서 if문을 이용해 좌표의 갯수를 기준으로 분기하여 Figure객체를 생성하던 것을 Map을 이용하여 객체를 생성하는 것으로 수정함

--- src/main/java/calculator/domain/FigureFactory.java
+++ src/main/java/calculator/domain/FigureFactory.java
@@ -1,37 +1,38 @@
 package calculator.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
 /**
  * @author soojinroh
  * @version 1.0 2019-05-22
  */
 public class FigureFactory {
+    private static final String EX_NO_SUCH_FIGURE_MESSAGE = "4각형까지만 지원되는 프로그램입니다.";
     private static final int POINT = 1;
     private static final int LINE = 2;
     private static final int TRIANGLE = 3;
     private static final int RECTANGLE = 4;
-    public static final String EX_NO_SUCH_FIGURE_MESSAGE = "4각형까지만 지원되는 프로그램입니다.";
-    private static FigureFactory instance = new FigureFactory();
+    private static final FigureFactory instance = new FigureFactory();
+    private final Map<Integer, Function<Coordinates, Figure>> figures = new HashMap<>();
 
     public static FigureFactory getInstance() {
         return instance;
     }
 
-    private FigureFactory() {}
+    private FigureFactory() { }
 
     public Figure create(Coordinates coordinates) {
-        if (coordinates.size() == POINT) {
-            return new Point(coordinates);
-        }
-        if (coordinates.size() == LINE) {
-            return new Line(coordinates);
-        }
-        if (coordinates.size() == TRIANGLE) {
-            return new Triangle(coordinates);
-        }
-        if (coordinates.size() == RECTANGLE) {
-            return new Rectangle((coordinates));
+        figures.put(POINT, Point::new);
+        figures.put(LINE, Line::new);
+        figures.put(TRIANGLE, Triangle::new);
+        figures.put(RECTANGLE, Rectangle::new);
+
+        if (figures.get(coordinates.size()) == null) {
+            throw new IllegalArgumentException(EX_NO_SUCH_FIGURE_MESSAGE);
         }
 
-        throw new IllegalArgumentException(EX_NO_SUCH_FIGURE_MESSAGE);
+        return figures.get(coordinates.size()).apply(coordinates);
     }
 }
