d4168865a99ae5b6b4b3e10035271c778e5b0079
이영현 <leey9494@icloud.com>
2019-05-27 20:39:56 +0900

    [MOD] 테스트 코드 수정

--- src/test/java/coordinate/domain/LineTest.java
+++ src/test/java/coordinate/domain/LineTest.java
@@ -9,20 +9,32 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class LineTest {
     @Test
     void 두_점을_가지고_있는_Line_생성() {
-        Line line = Line.create(Point.create(1, 4), Point.create(2, 6));
-        assertThat(line).isEqualTo(Line.create(Point.create(1, 4), Point.create(2, 6)));
+        Points points = Points.create();
+        points.addPoint(Point.create(1, 4));
+        points.addPoint(Point.create(2, 6));
+
+        Line line = Line.create(points);
+        assertThat(line).isEqualTo(Line.create(points));
     }
 
     @Test
     void 두_점사이의_거리_계산() {
-        Line line = Line.create(Point.create(4, 5), Point.create(6, 7));
+        Points points = Points.create();
+        points.addPoint(Point.create(4, 5));
+        points.addPoint(Point.create(6, 7));
+
+        Line line = Line.create(points);
         assertThat(line.calculateDistance()).isEqualTo(2.828427, offset(0.00099));
     }
 
     @Test
     void 점이_하나밖에_없을때() {
+        Points points = Points.create();
+        points.addPoint(null);
+        points.addPoint(Point.create(4, 5));
+
         assertThrows(IllegalArgumentException.class, () -> {
-            Line line = Line.create(null, Point.create(4, 5));
+            Line.create(points);
         });
     }
 }
--- src/test/java/coordinate/domain/RectangleTest.java
+++ src/test/java/coordinate/domain/RectangleTest.java
@@ -8,8 +8,13 @@ import static org.assertj.core.api.Assertions.offset;
 public class RectangleTest {
     @Test
     void 삼각형_넓이_구하기() {
-        Rectangle rectangle = Rectangle.create(Point.create(10, 10), Point.create(22, 10),
-                Point.create(22, 18), Point.create(10, 18));
+        Points points = Points.create();
+        points.addPoint(Point.create(10, 10));
+        points.addPoint(Point.create(22, 10));
+        points.addPoint(Point.create(22, 18));
+        points.addPoint(Point.create(10, 118));
+
+        Rectangle rectangle = Rectangle.create(points);
         assertThat(rectangle.getArea()).isEqualTo(96, offset(0.00099));
     }
 }
