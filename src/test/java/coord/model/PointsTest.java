14e6f29511eba62221b354d3cb0c47fe9c996a13
andole87 <andole87@gmail.com>
2019-05-28 20:45:29 +0900

    refactor: PointsTest.sortTest() 수정
    좌표 순서 변경

--- src/test/java/coord/model/PointsTest.java
+++ src/test/java/coord/model/PointsTest.java
@@ -10,6 +10,6 @@ class PointsTest {
     @Test
     void sortTest() {
         Points points = new Points(Arrays.asList(new Point(1, 3), new Point(4, 21), new Point(8, 21)));
-        assertEquals(Arrays.asList(new Point(4, 21), new Point(8, 21), new Point(1, 3)), points.getPoints());
+        assertEquals(Arrays.asList(new Point(8, 21), new Point(4, 21), new Point(1, 3)), points.getPoints());
     }
 }
