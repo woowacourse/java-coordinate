bf328edab0015d25d2ea9a66d4ffb527474f4316
yuyu154 <38104827+yuyu154@users.noreply.github.com>
2019-05-28 15:43:41 +0900

    [MOD] OutputView에서 예외 처리

--- src/main/java/coordinate/view/OutputView.java
+++ src/main/java/coordinate/view/OutputView.java
@@ -4,59 +4,68 @@ import coordinate.Figure;
 import coordinate.domain.Point;
 
 public class OutputView {
-	private static final int MAX_POINT = Point.MAX_POINT;
-	private static final int MIN_POINT = Point.MIN_POINT;
+    private static final String LINES = "----";
+    private static final String VERTICAL_BAR = "|";
+    private static final String FORMAT_NUMBER = "3";
+    private static final String STAR = "* ";
 
-	private OutputView() {
-	}
+    private static final int MAX_POINT = Point.MAX_POINT;
+    private static final int MIN_POINT = Point.MIN_POINT;
 
-	public static void showGraph(final Figure figure) {
-		showHorizontalLines(figure);
+    private OutputView() {
+    }
 
-		showLInes();
-		showBottomNumbers();
-	}
+    public static void showGraph(final Figure figure) {
+        showHorizontalLines(figure);
 
-	private static void showHorizontalLines(Figure figure) {
-		for (int row = MAX_POINT; row >= MIN_POINT; row--) {
-			System.out.print(String.format("%3s", row));
-			System.out.print("|");
-			showHorizontalOneLine(figure, row);
-			System.out.println();
-		}
-	}
+        showLInes();
+        showBottomNumbers();
+    }
 
-	private static void showHorizontalOneLine(Figure figure, int row) {
-		for (int col = MIN_POINT; col <= MAX_POINT; col++) {
-			showPoint(figure, row, col);
-		}
-	}
+    private static void showHorizontalLines(Figure figure) {
+        for (int row = MAX_POINT; row >= MIN_POINT; row--) {
+            System.out.print(String.format("%" + FORMAT_NUMBER + "s", row));
+            System.out.print(VERTICAL_BAR);
+            showHorizontalOneLine(figure, row);
+            System.out.println();
+        }
+    }
 
-	private static void showPoint(Figure figure, int row, int col) {
-		if (figure.hasPoint(Point.of(col, row))) {
-			System.out.print(String.format("%-3s", "* "));
-			return;
-		}
-		System.out.print(String.format("%3s", "   "));
-	}
+    private static void showHorizontalOneLine(Figure figure, int row) {
+        for (int col = MIN_POINT; col <= MAX_POINT; col++) {
+            showPoint(figure, row, col);
+        }
+    }
 
-	private static void showLInes() {
-		for (int col = MIN_POINT; col <= MAX_POINT; col++) {
-			System.out.print("----");
-		}
-		System.out.println();
-	}
+    private static void showPoint(Figure figure, int row, int col) {
+        if (figure.hasPoint(Point.of(col, row))) {
+            System.out.print(String.format("%-" + FORMAT_NUMBER + "s", STAR));
+            return;
+        }
+        System.out.print(String.format("%" + FORMAT_NUMBER + "s", "   "));
+    }
 
-	private static void showBottomNumbers() {
-		System.out.print("  ");
-		for (int col = MIN_POINT; col <= MAX_POINT; col++) {
-			System.out.print(String.format("%3s", col));
-		}
-		System.out.println();
-	}
+    private static void showLInes() {
+        for (int col = MIN_POINT; col <= MAX_POINT; col++) {
+            System.out.print(LINES);
+        }
+        System.out.println();
+    }
 
-	public static void showArea(final Figure figure) {
-		System.out.println();
-		System.out.println(figure.getName() + "는 " + String.format("%.3f", figure.getArea()) + "입니다");
-	}
+    private static void showBottomNumbers() {
+        System.out.print("  ");
+        for (int col = MIN_POINT; col <= MAX_POINT; col++) {
+            System.out.print(String.format("%" + FORMAT_NUMBER + "s", col));
+        }
+        System.out.println();
+    }
+
+    public static void showArea(final Figure figure) {
+        try {
+            System.out.println(figure.getName() + "는 "
+                    + String.format("%." + FORMAT_NUMBER + "f", figure.getArea()) + "입니다");
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
