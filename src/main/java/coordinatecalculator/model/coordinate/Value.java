244a3315ec16a43d2d8503923b33880a4738064c
stopsilver13 <stopsilver13>
2019-05-28 15:48:55 +0900

    [REVIEW] Add equals and hashCode at Values for checking same coordinate

--- src/main/java/coordinatecalculator/model/coordinate/Value.java
+++ src/main/java/coordinatecalculator/model/coordinate/Value.java
@@ -1,5 +1,7 @@
 package coordinatecalculator.model.coordinate;
 
+import java.util.Objects;
+
 public class Value {
     private static final int MIN_VALUE = 0;
     private static final int MAX_VALUE = 24;
@@ -19,5 +21,18 @@ public class Value {
 
     public int getValue() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Value value1 = (Value) o;
+        return value == value1.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
     }
 }
