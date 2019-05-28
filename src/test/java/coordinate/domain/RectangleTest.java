faa971d26a86c5d3ccc36797095a989abacc8891
starkim06 <starkim06@naver.com>
2019-05-28 14:16:23 +0900

    refactor: 공백 수정

--- src/test/java/coordinate/domain/RectangleTest.java
+++ src/test/java/coordinate/domain/RectangleTest.java
@@ -13,12 +13,12 @@ public class RectangleTest {
     @Test
     void 직사각형_아닌_입력() {
         List<Point> points = Arrays.asList(
-                new Point(10,10),
-                new Point(22,10),
-                new Point(22,18),
-                new Point(10,19)
+                new Point(10, 10),
+                new Point(22, 10),
+                new Point(22, 18),
+                new Point(10, 19)
         );
-        assertThrows(IllegalArgumentException.class,()->{
+        assertThrows(IllegalArgumentException.class, () -> {
             new Rectangle(points);
         });
     }
@@ -26,10 +26,10 @@ public class RectangleTest {
     @Test
     void 면적_계산() {
         List<Point> points = Arrays.asList(
-                new Point(10,10),
-                new Point(22,10),
-                new Point(22,18),
-                new Point(10,18)
+                new Point(10, 10),
+                new Point(22, 10),
+                new Point(22, 18),
+                new Point(10, 18)
         );
         Rectangle rectangle = new Rectangle(points);
         assertThat(rectangle.area()).isEqualTo(96, Offset.offset(0.0099));
