06644d48ee3228a2564463f2eb822f39eed10b6d
김성훈 <seonghun127@gmail.com>
2019-05-28 19:18:30 +0900

    feat : ShapeCreator 인터페이스 추가
    1. Funtion 인터페이스 사용대신 비슷한 역할을 하는 인터페이스 직접 생성한다
    	- Shape 타입의 도형을 생성하는 의미를 명확히 하기 위해

--- src/main/java/coordinate/domain/ShapeGenerator.java
+++ src/main/java/coordinate/domain/ShapeGenerator.java
@@ -10,16 +10,15 @@ public class ShapeGenerator {
     private static final int SIZE_OF_POINTS_FOR_TRIANGLE = 3;
     private static final int SIZE_OF_POINTS_FOR_RECTANGLE = 4;
 
-    private static final Map<Integer, Function<List<Point>, Shape>> shapeMapper;
+    private static final Map<Integer, ShapeCreator> shapeMapper = new HashMap<>();;
 
     static {
-        shapeMapper = new HashMap<>();
         shapeMapper.put(SIZE_OF_POINTS_FOR_LINE, Line::new);
         shapeMapper.put(SIZE_OF_POINTS_FOR_TRIANGLE, Triangle::new);
         shapeMapper.put(SIZE_OF_POINTS_FOR_RECTANGLE, Rectangle::new);
     }
 
     public static Shape create(List<Point> points) {
-        return shapeMapper.get(points.size()).apply(points);
+        return shapeMapper.get(points.size()).create(points);
     }
 }
