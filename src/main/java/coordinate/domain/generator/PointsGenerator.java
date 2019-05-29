fee5d49396ac4c0d51f78468f7d5d626fae5cf91
pkch93 <star_final@naver.com>
2019-05-29 14:54:42 +0900

    refactor: PointsGenerator.class 정규식 리펙토링
    String.matches()보다 Matcher의 matches()가 성능상 우수하므로 리펙토링

--- src/main/java/coordinate/domain/generator/PointsGenerator.java
+++ src/main/java/coordinate/domain/generator/PointsGenerator.java
@@ -6,15 +6,16 @@ import coordinate.domain.Points;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class PointsGenerator {
-    private static final String REGEX = "(\\([0-9]{1,2},[0-9]{1,2}\\)-)+\\([0-9]{1,2},[0-9]{1,2}\\)";
+    private static final Pattern POINTS_INPUT_REGEX = Pattern.compile("(\\([0-9]{1,2},[0-9]{1,2}\\)-)+\\([0-9]{1,2},[0-9]{1,2}\\)");
     private static final String DELIMITER = "-";
 
     private final String[] coordinates;
 
     public PointsGenerator(String coordinates) {
-        if (!coordinates.matches(REGEX)) {
+        if (!POINTS_INPUT_REGEX.matcher(coordinates).matches()) {
             throw new IllegalArgumentException("유효한 입력이 아닙니다. ex) (10,10)-(10,10)-(20,8)");
         }
         this.coordinates = Arrays.stream(coordinates.split(DELIMITER))
