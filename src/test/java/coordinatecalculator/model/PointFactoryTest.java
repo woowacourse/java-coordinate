71c70b0bf2dd19e891887fe73419b96949a75441
backlo <hjun4512@naver.com>
2019-05-29 15:00:58 +0900

    [REFACTOR] 자바컨벤션 정리

--- src/test/java/coordinatecalculator/model/PointFactoryTest.java
+++
@@ -1,9 +0,0 @@
-package coordinatecalculator.model;
-
-import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-
-public class PointFactoryTest {
-}
