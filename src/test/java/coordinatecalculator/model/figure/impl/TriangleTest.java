94675de15119c5d0cc61ca0da7340e006a5c5c48
stopsilver13 <stopsilver13>
2019-05-29 11:05:13 +0900

    [REVIEW] Add test code to TriangleTest

--- src/test/java/coordinatecalculator/model/figure/impl/TriangleTest.java
+++ src/test/java/coordinatecalculator/model/figure/impl/TriangleTest.java
@@ -18,4 +18,31 @@ public class TriangleTest {
         List<Coordinate> coordinates = Arrays.asList(c1, c2, c3);
         assertThat(new Triangle(coordinates).calculateArea()).isEqualTo(50);
     }
+
+    @Test
+    void 매우_작은_삼각형인_경우() {
+        Coordinate c1 = new Coordinate(0, 0);
+        Coordinate c2 = new Coordinate(0, 1);
+        Coordinate c3 = new Coordinate(1, 0);
+        List<Coordinate> coordinates = Arrays.asList(c1, c2, c3);
+        assertThat(new Triangle(coordinates).calculateArea()).isEqualTo(0.5);
+    }
+
+    @Test
+    void 극적인_둔각_삼각형인_경우() {
+        Coordinate c1 = new Coordinate(0, 0);
+        Coordinate c2 = new Coordinate(24, 0);
+        Coordinate c3 = new Coordinate(12, 1);
+        List<Coordinate> coordinates = Arrays.asList(c1, c2, c3);
+        assertThat(new Triangle(coordinates).calculateArea()).isEqualTo(12);
+    }
+
+    @Test
+    void 극적인_예각_삼각형인_경우() {
+        Coordinate c1 = new Coordinate(0, 0);
+        Coordinate c2 = new Coordinate(2, 0);
+        Coordinate c3 = new Coordinate(1, 24);
+        List<Coordinate> coordinates = Arrays.asList(c1, c2, c3);
+        assertThat(new Triangle(coordinates).calculateArea()).isEqualTo(24);
+    }
 }
