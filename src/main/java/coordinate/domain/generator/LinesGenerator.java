bb257620c5c6b5ff9ceae7f0588f9387794ac600
pkch93 <star_final@naver.com>
2019-05-29 17:16:07 +0900

    refactor: LinesGenerator
    LinesGenerator를 Interface로 사용하는 것 대신 도형에서 Points를 받아 Lines를
    만들도록 구현

--- src/main/java/coordinate/domain/generator/LinesGenerator.java
+++ src/main/java/coordinate/domain/generator/LinesGenerator.java
@@ -1,8 +1,25 @@
 package coordinate.domain.generator;
 
+import coordinate.domain.Line;
 import coordinate.domain.Lines;
+import coordinate.domain.Point;
+import coordinate.domain.Points;
 
-@FunctionalInterface
-public interface LinesGenerator {
-    Lines generate();
+import java.util.*;
+
+public class LinesGenerator {
+    public static Lines generateLines(Points points) {
+        List<Line> lines = new ArrayList<>();
+        for (int i = 0; i < points.size(); i++) {
+            generateLine(lines, points, i);
+        }
+        return new Lines(lines);
+    }
+
+    private static void generateLine(List<Line> lines, Points points, int target) {
+        Point point = points.get(target);
+        for (int i = target + 1; i < points.size(); i++) {
+            lines.add(new Line(Points.of(Arrays.asList(point, points.get(i)))));
+        }
+    }
 }
