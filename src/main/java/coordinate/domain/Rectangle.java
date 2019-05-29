0fd9d0fe66c3b643c0cdb61870e2b044e24d691c
wiseoldster <wiseoldster@gmail.com>
2019-05-29 14:54:45 +0900

    refactor Rectangle Class validateRectangle, area
    메소드 분리를 통해 역할 구분
    벡터 정렬 계산 반복이 추가 되는 문제 발생

--- src/main/java/coordinate/domain/Rectangle.java
+++ src/main/java/coordinate/domain/Rectangle.java
@@ -7,8 +7,9 @@ import java.util.stream.IntStream;
 
 public class Rectangle extends Figure implements AvailableArea {
     private static final int VALID_LENGTH_OF_POINTS = 4;
-
-    private Double area;
+    private static final int SIDE_1_VECTOR_INDEX = 0;
+    private static final int SIDE_2_VECTOR_INDEX = 1;
+    private static final int OPPOSITE_VECTOR_INDEX = 2;
 
     public Rectangle(Vertices vertices) {
         super(vertices, VALID_LENGTH_OF_POINTS);
@@ -16,24 +17,27 @@ public class Rectangle extends Figure implements AvailableArea {
     }
 
     private void validateRectanglePoints() {
-        List<Vector> vectorsFrom0 = IntStream.range(1, 4)
-                .mapToObj(i -> getVertices().vector(0, i))
-                .sorted(Vector::compareTo)
-                .collect(Collectors.toCollection(ArrayList::new));
-        Vector side1 = vectorsFrom0.get(0);
-        Vector side2 = vectorsFrom0.get(1);
-        Vector opposite = vectorsFrom0.get(2);
-
-        area = (double) Math.abs(side1.crossProduct(side2));
+        List<Vector> vectors = getVectors();
+        Vector side1 = vectors.get(SIDE_1_VECTOR_INDEX);
+        Vector side2 = vectors.get(SIDE_2_VECTOR_INDEX);
+        Vector opposite = vectors.get(OPPOSITE_VECTOR_INDEX);
 
         if ((side1.dotProduct(side2) != 0) || !side1.add(side2).equals(opposite)) {
             throw new IllegalArgumentException("직사각형이 아닙니다.");
         }
     }
 
+    private List<Vector> getVectors() {
+        return IntStream.range(1, 4)
+                    .mapToObj(i -> getVertices().vector(0, i))
+                    .sorted(Vector::compareTo)
+                    .collect(Collectors.toCollection(ArrayList::new));
+    }
+
     @Override
     public double area() {
-        return area;
+        List<Vector> vectors = getVectors();
+        return (double) Math.abs(vectors.get(SIDE_1_VECTOR_INDEX).crossProduct(vectors.get(SIDE_2_VECTOR_INDEX)));
     }
 
 }
