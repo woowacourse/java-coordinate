944e1cb2b1dc7a9a2ae63612e15e541627908f0f
dasistHYOJIN <hgenie14@gmail.com>
2019-05-29 11:03:09 +0900

    feat: change InputView to return primitive type data

--- src/main/java/coordinatecalculator/Application.java
+++ src/main/java/coordinatecalculator/Application.java
@@ -1,12 +1,14 @@
-package coordinatecalculator.controller;
+package coordinatecalculator;
 
+import coordinatecalculator.domain.FigureRouter;
 import coordinatecalculator.domain.parent.Figure;
+import coordinatecalculator.util.PointsGenerator;
 import coordinatecalculator.view.InputView;
 import coordinatecalculator.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        Figure figure = InputView.inputLocation();
+        Figure figure = FigureRouter.routing(PointsGenerator.create(InputView.inputLocation()));
 
         OutputView.printResult(figure);
     }
--- src/main/java/coordinatecalculator/view/InputView.java
+++ src/main/java/coordinatecalculator/view/InputView.java
@@ -1,17 +1,37 @@
 package coordinatecalculator.view;
 
-import coordinatecalculator.domain.FigureRouter;
-import coordinatecalculator.domain.parent.Figure;
-import coordinatecalculator.util.PointsGenerator;
-
 import java.util.*;
 
 public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final String INPUT_MESSAGE = "좌표를 입력하세요.";
+    private static final String INVALID_INPUT_MESSAGE = "입력된 값이 존재하지 않습니다.";
 
-    public static Figure inputLocation() {
+    public static String inputLocation() {
         System.out.println(INPUT_MESSAGE);
-        return FigureRouter.routing(PointsGenerator.create(SCANNER.nextLine()));
+
+        String input = SCANNER.nextLine();
+
+        validateInput(input);
+
+        return input;
     }
+
+    private static void validateInput(String input) {
+        validateNullInput(input);
+        validateEmptyInput(input);
+    }
+
+    private static void validateNullInput(String input) {
+        if (Objects.isNull(input)) {
+            throw new IllegalArgumentException(INVALID_INPUT_MESSAGE);
+        }
+    }
+
+    private static void validateEmptyInput(String input) {
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException(INVALID_INPUT_MESSAGE);
+        }
+    }
+
 }
