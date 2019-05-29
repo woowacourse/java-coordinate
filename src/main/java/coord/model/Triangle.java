dcc0574f8960c4f65ebc7df622157f296d6cd24e
andole87 <andole87@gmail.com>
2019-05-29 11:33:21 +0900

    refactor: Triangle 예외 추가
    넓이가 0인 삼각형은 예외로 규정한다.

--- src/main/java/coord/model/Triangle.java
+++ src/main/java/coord/model/Triangle.java
@@ -6,6 +6,18 @@ public class Triangle extends Figure {
 
     protected Triangle(Points points) {
         super(points);
+        validate(points);
+    }
+
+    private void validate(Points points) {
+        Point A = points.get(0);
+        Point B = points.get(1);
+        Point C = points.get(2);
+
+        if ((A.y - B.y) / (A.x - B.x) == (B.y - C.y) / (B.x - C.x)) {
+            throw new IllegalArgumentException();
+        }
+
     }
 
     @Override
