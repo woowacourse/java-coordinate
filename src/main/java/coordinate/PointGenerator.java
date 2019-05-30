b3ecf9a854b1d059288f657b26dfc924cd8265cd
andole87 <andole87@gmail.com>
2019-05-30 10:46:47 +0900

    feat: 좌표 예외 추가
    숫자가 짝수가 아닐 경우.
    입력 형식이 잘못된 경우.

--- src/main/java/coordinate/PointGenerator.java
+++ src/main/java/coordinate/PointGenerator.java
@@ -6,10 +6,26 @@ import java.util.List;
 public class PointGenerator {
 
     public static List<Point> trans(List<Integer> numbers) {
+        List<Point> points = getPoints(numbers);
+        return points;
+    }
+
+
+    private static List<Point> getPoints(List<Integer> numbers) {
+        validate(numbers);
         List<Point> points = new ArrayList<>();
         for (int i = 0; i < numbers.size(); i += 2) {
             points.add(new Point(numbers.get(i), numbers.get(i + 1)));
         }
         return points;
+    }
+
+    private static void validate(List<Integer> numbers) {
+        if (numbers.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        if (numbers.size() % 2 != 0) {
+            throw new IllegalArgumentException();
+        }
     }
 }
--- src/test/java/coordinate/PointGeneratorTest.java
+++ src/test/java/coordinate/PointGeneratorTest.java
@@ -8,11 +8,24 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PointGeneratorTest {
-    String input = "(1,2)-(3,4)-(5,6)";
+
 
     @Test
     void 생성() {
+        String input = "(1,2)-(3,4)-(5,6)";
         assertEquals(Arrays.asList(new Point(1, 2), new Point(3, 4), new Point(5, 6)),
                 PointGenerator.trans(InputView.parse(input)));
+    }
+
+    @Test
+    void 좌표_예외1() {
+        String input = "(1,2)-(3,4)-(5,";
+        assertThrows(IllegalArgumentException.class, () -> PointGenerator.trans(InputView.parse(input)));
+    }
+
+    @Test
+    void 좌표_예외2() {
+        String input = "(1,2)(3,4)";
+        assertThrows(IllegalArgumentException.class, () -> PointGenerator.trans(InputView.parse(input)));
     }
 }
