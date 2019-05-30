1ee434f4a8e19fa54e2d18d4745a4c10aecbebf8
andole87 <andole87@gmail.com>
2019-05-30 09:48:51 +0900

    feat: 입력 좌표 파싱
    숫자 배열로 파싱

--- src/main/java/coordinate/view/InputView.java
+++ src/main/java/coordinate/view/InputView.java
@@ -1,5 +1,6 @@
 package coordinate.view;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,5 +8,14 @@ public class InputView {
 
     public static List<String> split(String text) {
         return Arrays.asList(text.split("-"));
+    }
+
+    public static List<Integer> parse(String input) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String text : split(input)) {
+            text = text.replace("(", "").replace(")", "");
+            Arrays.stream(text.split(",")).forEach(n -> numbers.add(Integer.parseInt(n)));
+        }
+        return numbers;
     }
 }
--- src/test/java/coordinate/view/InputViewTest.java
+++ src/test/java/coordinate/view/InputViewTest.java
@@ -7,10 +7,15 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 class InputViewTest {
+    private String input = "(1,2)-(3,4)-(5,6)";
 
     @Test
     void 좌표_분리() {
-        String input = "(1,2)-(3,4)-(5,6)";
         assertEquals(Arrays.asList("(1,2)", "(3,4)", "(5,6)"), InputView.split(input));
+    }
+
+    @Test
+    void 좌표_숫자로_변환() {
+        assertEquals(Arrays.asList(1, 2, 3, 4, 5, 6), InputView.parse(input));
     }
 }
