64dd696d24f1f0ab93b2fb37bfd17f0b480b4bc9
dasistHYOJIN <hgenie14@gmail.com>
2019-05-30 10:44:38 +0900

    refactor: change enum variable name

--- src/main/java/coordinatecalculator/domain/FigureType.java
+++ src/main/java/coordinatecalculator/domain/FigureType.java
@@ -2,27 +2,27 @@ package coordinatecalculator.domain;
 
 import coordinatecalculator.domain.parent.Figure;
 
-public enum CountOfPoints {
-    _2("LINE") {
+public enum FigureType {
+    LINE("LINE") {
         @Override
         public Figure getFigure(Points points) {
             return Line.newInstance(points);
         }
     },
-    _3("TRIANGLE") {
+    TRIANGLE("TRIANGLE") {
         @Override
         public Figure getFigure(Points points) {
             return Triangle.newInstance(points);
         }
     },
-    _4("RECTANGLE") {
+    RECTANGLE("RECTANGLE") {
         @Override
         public Figure getFigure(Points points) {
             return Rectangle.newInstance(points);
         }
     };
 
-    CountOfPoints(String countOfPoints) {
+    FigureType(String countOfPoints) {
     }
 
     public abstract Figure getFigure(Points points);
