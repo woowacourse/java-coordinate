d2b8051596cec7cabac3126c8a6736960e92bd31
choihz <choixhz@gmail.com>
2019-06-03 15:46:13 +0900

    Fix FigureGeneratorTest Class
    깨지는 테스트 수정 및 테스트 개선

--- src/test/java/coordinate/domain/FigureGeneratorTest.java
+++ src/test/java/coordinate/domain/FigureGeneratorTest.java
@@ -14,26 +14,35 @@ public class FigureGeneratorTest {
     public void Point_2개_입력됐을_때_Line을_만드는지_확인() {
         List<Point> points = new ArrayList<>(Arrays.asList(new Point(1, 1), new Point(2, 2)));
 
-        assertThat(new FigureGenerator().generate(points) instanceof Line).isTrue();
+        assertThat(new FigureGenerator().generate(points)).isExactlyInstanceOf(Line.class);
     }
 
     @Test
     public void Point_3개_입력됐을_때_Triangle을_만드는지_확인() {
-        List<Point> points = new ArrayList<>(Arrays.asList(new Point(1, 1), new Point(2, 2), new Point(3, 3)));
+        List<Point> points = new ArrayList<>(Arrays.asList(new Point(1, 1), new Point(2, 2), new Point(4, 3)));
 
-        assertThat(new FigureGenerator().generate(points) instanceof Triangle).isTrue();
+        assertThat(new FigureGenerator().generate(points)).isExactlyInstanceOf(Triangle.class);
     }
 
     @Test
     public void Point_4개_입력됐을_때_Rectangle을_만드는지_확인() {
         List<Point> points = new ArrayList<>(Arrays.asList(new Point(1, 1), new Point(1, 2), new Point(2, 1), new Point(2, 2)));
 
-        assertThat(new FigureGenerator().generate(points) instanceof Rectangle).isTrue();
+        assertThat(new FigureGenerator().generate(points)).isExactlyInstanceOf(Rectangle.class);
     }
 
     @Test
     public void Point_5개_입력됐을_때_예외를_던지는지_확인() {
         List<Point> points = new ArrayList<>(Arrays.asList(new Point(1, 1), new Point(1, 2), new Point(2, 1), new Point(2, 2), new Point(5, 5)));
+
+        assertThatThrownBy(() -> {
+            new FigureGenerator().generate(points);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 세_점이_한_직선상에_있을_때_예외를_던지는지_확인() {
+        List<Point> points = new ArrayList<>(Arrays.asList(new Point(1, 1), new Point(2, 2), new Point(3, 3)));
 
         assertThatThrownBy(() -> {
             new FigureGenerator().generate(points);
