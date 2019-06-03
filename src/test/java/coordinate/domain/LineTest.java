9480d09d28cdc94381bb7c9a3f8e31056eda85a8
choihz <choixhz@gmail.com>
2019-06-03 15:48:27 +0900

    Fix LineTest Class
    테스트 코드 개선

--- src/test/java/coordinate/domain/LineTest.java
+++ src/test/java/coordinate/domain/LineTest.java
@@ -1,6 +1,5 @@
 package coordinate.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -10,14 +9,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.*;
 
 public class LineTest {
-    private Line line;
-
-    @BeforeEach
-    public void setUp() {
-        List<Point> points = new ArrayList<>(Arrays.asList(new Point(1, 1), new Point(2, 2)));
-        line = new Line(points);
-    }
-
     @Test
     public void Point가_2개_미만_들어올_경우() {
         List<Point> points = new ArrayList<>(Arrays.asList(new Point(1, 1)));
@@ -38,6 +29,9 @@ public class LineTest {
 
     @Test
     public void lengthTest() {
+        List<Point> points = new ArrayList<>(Arrays.asList(new Point(1, 1), new Point(2, 2)));
+        Line line = new Line(points);
+
         assertThat(line.length()).isEqualTo(1.414, offset(0.00099));
     }
 }
