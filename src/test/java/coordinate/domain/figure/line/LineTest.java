90ba02c7a2a91413240b19418cf4e9143c6370d7
ddu0422 <ddu0422@naver.com>
2019-06-12 21:08:26 +0900

    add : Line 생성 및 길이 구하기

---
+++ src/test/java/coordinate/domain/figure/line/LineTest.java
@@ -0,0 +1,41 @@
+package coordinate.domain.figure.line;
+
+import coordinate.domain.figure.InvalidAbstractFigureException;
+import coordinate.domain.point.Point;
+import org.assertj.core.data.Offset;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class LineTest {
+    @Test
+    void 라인_생성() {
+        Line line = new Line(Arrays.asList(
+                new Point(0,0),
+                new Point(1,1)
+        ));
+        assertThat(line).isEqualTo(new Line(Arrays.asList(
+                new Point(0,0),
+                new Point(1,1)
+        )));
+    }
+
+    @Test
+    void 라인_생성_시_중복된_점_존재() {
+        assertThrows(InvalidAbstractFigureException.class, () -> new Line(Arrays.asList(
+                new Point(0,0),
+                new Point(0,0)
+        )));
+    }
+
+    @Test
+    void 라인의_길이_계산() {
+        List<Point> point = Arrays.asList(new Point(0,0), new Point(1, 1));
+        Line line = new Line(point);
+        assertThat(line.area()).isEqualTo(1.414, Offset.offset(0.001));
+    }
+}
