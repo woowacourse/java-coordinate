1891aa823411af8b56b6ef32917b1d878906bab4
ddu0422 <ddu0422@naver.com>
2019-06-12 21:11:31 +0900

    add : 구현 클래스 오류 클래스 작성

---
+++ src/main/java/coordinate/domain/figure/InvalidAbstractFigureException.java
@@ -0,0 +1,7 @@
+package coordinate.domain.figure;
+
+public class InvalidAbstractFigureException extends RuntimeException {
+    public InvalidAbstractFigureException(String msg) {
+        super(msg);
+    }
+}
